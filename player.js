4976ae4db6ab6b23d5b03b46ae9c96305552a14f
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-27 14:08:55 -0500

    add enemy/player/charcater class

---
+++ player.js
@@ -0,0 +1,80 @@
+class character(player,enemy){
+    this.type = [earth,air,fire,water];
+    this.health = 100;
+    this.firstAttack=[]
+    this.secondAttack=[]
+    this.thirdAttack=[]
+}
+
+class enemy extends earth{
+    constructor(type){
+        super(type);
+        this.firstAttack=;
+        this.secondAttack=;
+        this.thirdAttack=;
+    }
+}
+
+class enemy extends air{
+    constructor(type){
+        super(type);
+        this.firstAttack=;
+        this.secondAttack=;
+        this.thirdAttack=;
+    }
+}
+
+class enemy extends fire{
+    constructor(type){
+        super(type);
+        this.firstAttack=;
+        this.secondAttack=;
+        this.thirdAttack=;
+    }
+}
+
+class enemy extends water{
+    constructor(type){
+        super(type);
+        this.firstAttack=;
+        this.secondAttack=;
+        this.thirdAttack=;
+    }
+}
+
+class enemy extends water{
+    constructor(type){
+        super(type);
+        this.firstAttack=;
+        this.secondAttack=;
+        this.thirdAttack=;
+    }
+}
+
+class player extends earth{
+    constructor(type){
+        super(type);
+        this.firstAttack=;
+        this.secondAttack=;
+        this.thirdAttack=;
+    }
+}
+
+class player extends fire{
+    constructor(type){
+        super(type);
+        this.firstAttack=;
+        this.secondAttack=;
+        this.thirdAttack=;
+    }
+}
+
+class player extends air{
+    constructor(type){
+        super(type);
+        this.firstAttack=;
+        this.secondAttack=;
+        this.thirdAttack=;
+    }
+}
+
